1eb46841c9c0a8180cf2210c4f34c7b9dd61ba21
zhi.zhou <zhi.zhou@chaitin.com>
2016-04-17 16:36:19 +0800

    add tls banner grabbing

--- tests/grab.test.js
+++ tests/grab.test.js
@@ -1,23 +1,50 @@
 'use strict';
 
 const grabber = require('../lib');
-const chai = require('chai'), expect = chai.expect;
+const chai = require('chai'),
+  expect = chai.expect;
+
+const net = require('net'),
+  tls = require('tls'),
+  https = require('https'),
+  fs = require('fs'),
+  path = require('path');
 
 describe('homepage', () => {
   it('should read banner', done => {
-    let bannerExample = 'This-Is-A-TCP-Banner\n';
-    let dummy = require('net').createServer((socket) => {
-      socket.end(bannerExample);
-    }).listen(() => {
-      let address = dummy.address();
-      grabber.grab('127.0.0.1', address.port)
-        .run()
-        .then(data => {
-          expect(data.toString()).to.equals(bannerExample)
-          done();
-        })
-        .catch(done);
-    });
+    let randomBanner = 'This-Is-A-TCP-Banner\n' + Math.random().toString(16);
+    let server = net.createServer(socket => socket.end(randomBanner))
+      .listen(() => {
+        grabber.grab('127.0.0.1', server.address().port)
+          .run()
+          .then(result => {
+            expect(result.banner.toString()).to.equals(randomBanner);
+            done();
+          })
+          .catch(done);
+      });
+  });
+
+  it('should support tls', done => {
+    let key = name => path.join(__dirname, 'keys', name);
+    let options = {
+      key: fs.readFileSync(key('server-key.pem')),
+      ca: [fs.readFileSync(key('ca-key.pem'))],
+      cert: fs.readFileSync(key('server-cert.pem'))
+    };
+
+    let randomBanner = 'This-Is-A-Server-With-TLS\n' + Math.random().toString(16);
+    let server = tls.createServer(options, socket => socket.end(randomBanner))
+      .listen(() => {
+        grabber.grab('127.0.0.1', server.address().port, { tls: true })
+          .run()
+          .then(result => {
+            expect(result.banner.toString()).to.equals(randomBanner);
+            expect(result).to.have.property('certificate');
+            done();
+          })
+          .catch(done);
+      });
+
   });
 });
-

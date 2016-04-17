c33c53116ac2805b142aaa0f9bde014d94e6d603
zhi.zhou <zhi.zhou@chaitin.com>
2016-04-17 19:12:26 +0800

    send payload when avaliable add travis-ci and coveralls

--- tests/grab.test.js
+++ tests/grab.test.js
@@ -10,7 +10,7 @@ const net = require('net'),
   fs = require('fs'),
   path = require('path');
 
-describe('homepage', () => {
+describe('grabber', () => {
   it('should read banner', done => {
     let randomBanner = 'This-Is-A-TCP-Banner\n' + Math.random().toString(16);
     let server = net.createServer(socket => socket.end(randomBanner))
@@ -45,6 +45,46 @@ describe('homepage', () => {
           })
           .catch(done);
       });
-
   });
+
+  it('should support payload', done => {
+    let randomBanner = 'This-Is-A-TCP-Banner\n' + Math.random().toString(16);
+    let payload = new Buffer(Math.random().toString(36));
+    let server = net.createServer(socket =>
+        socket.on('data', data => {
+          if (data.compare(payload) === 0)
+            socket.end(randomBanner)
+          else
+            socket.end()
+        }))
+      .listen(() => {
+        grabber.grab('127.0.0.1', server.address().port)
+          .payload(payload)
+          .run()
+          .then(result => {
+            expect(result.banner.toString()).to.equals(randomBanner);
+            done();
+          })
+          .catch(done);
+      });
+  });
+
+  it('should reject when time reached', done => {
+    grabber.grab('8.8.8.8', 80, { timeout: 10 })
+      .run()
+      .catch(err => {
+        expect(err.toString()).to.match(/^Error: Unable to read banner/);
+        done();
+      });
+  });
+
+});
+
+describe('buffer escape', () => {
+  it('should escape unprintable chars', done => {
+    expect(new Buffer([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]).toEscaped()).to.equals('buffer');
+    expect(new Buffer([0x00, 0x09, 0x20, 0x0A, 0xFF]).toEscaped()).to.equals('\\x00\\t \n\\xFF');
+
+    done();
+  })
 });

cd13bbdd24a318a2840b7865779febf5ea779114
zhi.zhou <zhi.zhou@chaitin.com>
2016-05-11 16:33:21 +0800

    add after param

--- cli.js
+++ cli.js
@@ -17,6 +17,7 @@ program
   .option('--parser <parser>', 'parse with nmap rule')
   .option('--encoding [encoding]', 'encode banner', /^(base64|hex)$/)
   .option('--timeout <timeout>', 'timeout', parseInt)
+  .option('--after', 'grab after zmap has been finished')
   .parse(process.argv)
 
 let parse = program.parser ? (() => {
@@ -25,22 +26,31 @@ let parse = program.parser ? (() => {
       (Object.keys(parsed).forEach(key => (data[key] = parsed[key])), data))
   })() : data => data
 
-process.stdin.on('data', buf =>
-  buf.toString().split('\n').filter(ip => ip).forEach(ip => 
-    grabber.grab(ip, program.port, {
-        tls: program.tls, payload: program.payload, timeout: program.timeout
-      })
-      .run()
-      .then(parse)
-      .then(data => {
-        data.ip = ip
-        data.banner = program.encoding ? 
-          data.banner.toString(program.encoding) :
-          data.banner.toEscaped()
-        return data
-      })
-      .then(JSON.stringify)
-      .then(console.log)
-      .catch(err => console.log({ip: ip, error: err.message}))
-  )
-)
+const grab = ip => grabber.grab(ip, program.port, {
+    tls: program.tls,
+    payload: program.payload,
+    timeout: program.timeout
+  })
+  .run()
+  .then(parse)
+  .then(data => {
+    data.ip = ip
+    data.banner = program.encoding ? 
+      data.banner.toString(program.encoding) :
+      data.banner.toEscaped()
+    return data
+  })
+  .then(JSON.stringify)
+  .then(console.log)
+  .catch(err => console.log({ip: ip, error: err.message}))
+
+if (program.after) {
+  let list = ''
+  process.stdin
+    .on('data', buf => list += buf.toString())
+    .on('close', () => list.split('\n').filter(ip => ip).forEach(grab))
+
+} else {
+  process.stdin
+    .on('data', buf => buf.toString().split('\n').filter(ip => ip).forEach(grab))
+}
